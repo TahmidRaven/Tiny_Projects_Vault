897b5e0f3e645b69a031c67d7ff17adfdde6ab29
Raven Endless <103381750+TahmidRaven@users.noreply.github.com>
2023-12-18 00:51:10 +0600

    QR code generator is up and running

---
+++ QR_Code_Generator/main.py
@@ -0,0 +1,24 @@
+import os
+import qrcode
+
+
+output_directory = "C:/CODE/TahmidRaven/Tiny_Projects_Vault/QR_Code_Generator/images"
+os.makedirs(output_directory, exist_ok=True)
+
+makeQR = input("Enter the link that you want to generate:")
+filename = input("Enter the name of your QR code (including extension, e.g., qrcode.jpg): ")
+
+if not filename.endswith((".jpg", ".png", ".gif")):
+    filename += ".jpg"
+
+myqr = qrcode.QRCode(version=1,
+                     error_correction=qrcode.constants.ERROR_CORRECT_L,
+                     box_size=10,
+                     border=4)
+
+# myqr.add_data("https://github.com/TahmidRaven")
+myqr.add_data(makeQR)
+
+img = myqr.make_image(fill_color="#932F8C", back_color="white")  # hexcode #932F8C #441d4b
+
+img.save(os.path.join(output_directory, filename))
---
+++ QR_Code_Generator/readme.md
@@ -0,0 +1,9 @@
+# QR Code Generator
+## This is a python script that creates a QR Code
+
+It's a simple script; however, I plan on doing something more sophisticated with it later on. 
+
+### Features:
+1. user inputs have been provided for both the link and the naming of the image file
+2. user can change the location where it can be saved
+3. extension handeling is enabled
